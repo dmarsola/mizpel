a4e759fd21d3eac71f0c22ed5f04f3a566dd6a54
dmarsola <marsola.douglas@cegep-heritage.qc.ca>
2019-07-27 09:29:03 -0400

    Getting ready for pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name='mizpel',
+  version='0.1',
+  scripts=['mizpel'],
+  author="Doug Marsola",
+  author_email="douglasmarsola@gmail.com",
+  description="Spell checker, word suggestion and dictionary",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/dmarsola/mizpel",
+  # packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ]
+)
--- strings_test.py
+++ strings_test.py
@@ -3,39 +3,39 @@ from WordDictionary import WordDictionary
 
 
 if __name__ == "__main__":
-    test_words = ['bluber', 'agnostyc', 'hopelesslly', 'zoro', 'myopy',
-                  'miopy', 'flowr', 'flower', 'klower', 'floewr', 'flowre',
-                  'shavel', 'tre']
+  test_words = ['bluber', 'agnostyc', 'hopelesslly', 'zoro', 'myopy',
+                'miopy', 'flowr', 'flower', 'klower', 'floewr', 'flowre',
+                'shavel', 'tre']
 
-    # investigate why floewr does not bring any results.
+  # investigate why floewr does not bring any results.
 
-    # Using the WordDictionary class
-    wd = WordDictionary()
-    # wd = WordDictionary("wlist_match5.txt")
-    # wd = WordDictionary("words_alpha.txt")
+  # Using the WordDictionary class
+  wd = WordDictionary()
+  # wd = WordDictionary("wlist_match5.txt")
+  # wd = WordDictionary("words_alpha.txt")
 
-    for word in test_words:
-        start = datetime.datetime.now()
-        similar_words = []
-        if word not in wd:
-            similar_words = wd.get_similar_words(word)
-        else:
-            print(f'{word} is in the dictionary')
-        end = datetime.datetime.now()
-        print(f'It took {end-start} milliseconds to search {word}: {similar_words}')
+  for word in test_words:
+    start = datetime.datetime.now()
+    similar_words = []
+    if word not in wd:
+      similar_words = wd.get_similar_words(word)
+    else:
+      print(f'{word} is in the dictionary')
+    end = datetime.datetime.now()
+    print(f'It took {end-start} milliseconds to search {word}: {similar_words}')
 
-    print(f'\nadding miopy: {wd.append("miopy")}')
-    print(f'adding miopy again: {wd.append("miopy")}')
-    print(f'is miopy in wd: {"miopy" in wd}')
-    print(f'removing miopy: {wd.remove("miopy")}')
-    print(f'is miopy in wd: {"miopy" in wd}')
+  print(f'\nadding miopy: {wd.append("miopy")}')
+  print(f'adding miopy again: {wd.append("miopy")}')
+  print(f'is miopy in wd: {"miopy" in wd}')
+  print(f'removing miopy: {wd.remove("miopy")}')
+  print(f'is miopy in wd: {"miopy" in wd}')
 
-    print("\n2nd round.")
-    for word in test_words:
-        # print(f'\nFinding similar words to {word}...')
-        start = datetime.datetime.now()
-        similar_words = []
-        if word not in wd:
-            similar_words = wd.get_similar_words(word)
-        end = datetime.datetime.now()
-        print(f'It took {end-start} milliseconds to search {word}: {similar_words}')
+  print("\n2nd round.")
+  for word in test_words:
+    # print(f'\nFinding similar words to {word}...')
+    start = datetime.datetime.now()
+    similar_words = []
+    if word not in wd:
+      similar_words = wd.get_similar_words(word)
+    end = datetime.datetime.now()
+    print(f'It took {end-start} milliseconds to search {word}: {similar_words}')
